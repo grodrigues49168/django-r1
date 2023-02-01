c208c8793996e2e110c7955e24b9168023615e00
grodrigues49168 <init.defaultBranch>
2023-02-01 09:42:24 -0300

    v0.0.12

--- recipes/urls.py
+++ recipes/urls.py
@@ -1,9 +1,13 @@
 from django.urls import path
-from recipes.views import home, sobre, contato, receitas
+from recipes.views import home, sobre, contatos, contatos_2, contatos_3, receitas, receitas_2, receitas_3
 
 urlpatterns = [
     path('', home),
     path('sobre/', sobre),
-    path('contato/', contato, name='contato'),
-    path('receita/', receitas, name='receitas')
+    path('contatos/', contatos, name='contatos'),
+    path('contatos_2/', contatos_2, name='contatos_2'),
+    path('contatos_3/', contatos_3, name='contatos_3'),
+    path('receitas/', receitas, name='receitas'),
+    path('receitas_2/', receitas_2, name='receitas_2'),
+    path('receitas_3/', receitas_3, name='receitas_3')
 ]
--- recipes/views.py
+++ recipes/views.py
@@ -6,16 +6,26 @@ from django.http import HttpResponse
 # HTTP RESPONSE
 
 def home(request):
-    return render(request, 'recipes/pages/home.html', context={'name': 'Gabriel Rodrigues'})
+    return render(request, 'recipes/pages/home.html', context={'name': 'Gabriel'})
 
 def sobre(request):
     return render(request,'sobre.html')
 
-def contato(request):
-    return render(request,'recipes/pages/contato.html', context={'name': 'Gabriel Rodrigues'})
+def contatos(request):
+  return render(request, 'recipes/pages/contatos.html', context={'name': 'Gabriel'})
 
+def contatos_2(request):
+  return render(request, 'recipes/pages/contatos_2.html', context={'name': 'Gabriel'})
+
+def contatos_3(request):
+  return render(request, 'recipes/pages/contatos_3.html', context={'name': 'gabriel'})
 def receitas(request):
-    return render(request, 'recipes/pages/receitas.html', context={'name': 'Gabriel Rodrigues'})
+    return render(request, 'recipes/pages/receitas.html', context={'name': 'Gabriel'})
 
+def receitas_2(request):
+    return render(request, 'recipes/pages/receitas_2.html', context={'name': 'gabriel'})
+
+def receitas_3(request):
+    return render(request, 'recipes/pages/receitas_3.html', context={'name': 'gabriel'})
 
 # Create your views here.
