2485659fccb0685049436a8ee63ddb9030b357c0
Gabriel Rodrigues Freitas <20201198060015@ifrn.local>
2023-02-03 10:19:34 -0300

    v0.0.30

--- prjDjango1/settings.py
+++ prjDjango1/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'HmXDlkbaYdqcdZrxLmrhWoEuXM7WYpz272eC0MdLmPXEYLMmt8hddR3RwgGP'
+SECRET_KEY = 'HmXDlkbaYdqcdZrxLmrhWoEuXM7WYpz272eC0MdLmPXEYLMmt8hddR3RwgGP',
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
