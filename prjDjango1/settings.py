49e637dbbd86f3890664acbcce3e633a86e9c2f6
Gabriel Rodrigues Freitas <20201198060015@ifrn.local>
2023-02-03 10:15:43 -0300

    v0.0.31

--- prjDjango1/settings.py
+++ prjDjango1/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'HmXDlkbaYdqcdZrxLmrhWoEuXM7WYpz272eC0MdLmPXEYLMmt8hddR3RwgGP'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['localhost','https://newdjango-r1.azurewebsites.net', 'newdjango-r1.azurewebsites.net']
 # Application definition
